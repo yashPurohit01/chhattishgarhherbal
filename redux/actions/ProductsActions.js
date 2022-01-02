cba5b3dbb34ae6af2e431e41445aafae0dac12c5
Yash Mukund Purohit <j.samkumar@shivila.com>
2022-01-03 00:47:23 +0530

    https added

--- redux/actions/ProductsActions.js
+++ redux/actions/ProductsActions.js
@@ -4,7 +4,7 @@ import { PRODUCTS_REQUEST_FAIL, PRODUCTS_REQUEST_SUCCESS } from "../constant"
 
 
 export const ProductsAction = () => (dispatch) =>{
-     const ProductUrl = "http://cgherbalbackend.herokuapp.com/products"
+     const ProductUrl = "https://cgherbalbackend.herokuapp.com/products"
      axios.get(ProductUrl)
      .then(res =>{
          console.log(res)
